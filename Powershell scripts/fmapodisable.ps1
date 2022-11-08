67d3a24a2dfbf0529dc34b0197e31390b2ce2cf0
Dan Crain <dcrain@oceusnetworks.com>
2022-11-08 10:22:43 -0600

    fmapodisable

---
+++ Powershell scripts/fmapodisable.ps1
@@ -0,0 +1,6 @@
+﻿$FMService = Get-Service | where Name -eq "FMAPOService"
+	if ($FMService) {"FM APOService Found.. removing"} 
+		stop-service -name fmaposervice -force
+		set-service -name fmaposervice -startuptype disabled
+	else {"FM APOService NOT Found.. exiting"}
+		exit
